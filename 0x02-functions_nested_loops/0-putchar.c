daf85436aa91310f5281cd2f3441e122dd6fd4a9
Davelocs <davembugua52@gmail.com>
2023-02-21 04:52:46 -0800

    nested

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (void)
+{
+   
+    // Get the character to be written
+    char ch = 'B';
+  
+    // Write the Character to stdout
+    putchar(ch);
+  
+    return (0);
+
+}	
